35847bae36e94c9279dd173a09f503a8d57ffe13
Kevin P <127263761+desl8@users.noreply.github.com>
2024-02-28 17:46:27 -0800

    v1.1

--- kc4.py
+++ kc4.py
@@ -19,10 +19,14 @@ class GameBoard:
     """A class for a Connect Four Gameboard"""
 
     def __init__(self, w=7, h=6, streak=4, board=None) -> None:
-        """
-        constructor method with appropriate initialization of instance variables
-        Board is a rudimentary (nested list) 2d array of 0s, 1s, and 2s to indicate the
-        occupant of cells on the board
+        """The initialization of the gameboard class
+        All default values match those of a typical Connect Four board
+
+        Args:
+            w (int, optional): The width of the board. Defaults to 7.
+            h (int, optional): The height of the board. Defaults to 6.
+            streak (int, optional): The streak required to win the game. Defaults to 4.
+            board (_type_, optional): A preexisting game board (integer array), if there is any. Defaults to None.
         """
         self.w = w
         self.h = h
@@ -33,7 +37,11 @@ class GameBoard:
             self.board = board
 
     def display(self, do_ascii=False) -> None:
-        """displays the gameboard"""
+        """Prints the gameboard to the terminal
+
+        Args:
+            do_ascii (bool, optional): Whether to return a stylized version of the board rather than the default number array. Defaults to False.
+        """
         if do_ascii:
             asciiboard = copy.deepcopy(self.board)
             for i in range(self.h):
@@ -50,26 +58,70 @@ class GameBoard:
             for i in range(self.h):
                 print(self.board[i])
 
-    def get_occupant(self, r: int, c: int) -> int:
-        """to return a "0", "1" or "2" indicating which player ("0" if space is empty) occupies the current r (row) and c (column) location. r is the row (0 at top) c is the column (0 at left)"""
-        return self.board[r][c]
+    def get_occupant(self, coords: tuple) -> int:
+        """Returns the current occupant of a cell
+
+        Args:
+            coords (tuple): The row-column coordinates of the cell
+
+        Returns:
+            int: The current occupant (0 for no occupant, 1 for P1, 2 for P2)
+        """
+        return self.board[coords[0]][coords[1]]
+
+    def get_dims(self) -> tuple:
+        """Returns the dimensions of the board
+
+        Returns:
+            tuple: (height, width) of the board
+        """
+        return (self.h, self.w)
+
+    def get_board(self, do_ascii=False) -> list:
+        """Returns the simple board object in its list form
+
+        Args:
+            do_ascii (bool, optional): Whether to return a stylized version of the board rather than the default number array. Defaults to False.
+
+        Returns:
+            list: The board object in its array form
+        """
+        if do_ascii:
+            asciiboard = copy.deepcopy(self.board)
+            for i in range(self.h):
+                for j in range(self.w):
+                    if asciiboard[i][j] == 0:
+                        asciiboard[i][j] = " "
+                    if asciiboard[i][j] == 1:
+                        asciiboard[i][j] = "●"
+                    if asciiboard[i][j] == 2:
+                        asciiboard[i][j] = "○"
+            return asciiboard
+        else:
+            return self.board
 
     def add_piece(self, c: int, p: int) -> bool:
-        # to return True if successful for adding a p (player "1" or "2") piece to c
-        # (column) location. False otherwise
-        # c is the column (0 at left)
-        # p is the player (1 or 2)
-        didplace = False
+        """Returns true if piece is placed successfully, false otherwise
+
+        Args:
+            c (int): The column to place in
+            p (int): The player who is placing a piece
+
+        Returns:
+            bool: Whether the piece was placed or not
+        """
         for i in range(self.h):
             if self.board[(self.h - i - 1)][c] == 0:
                 self.board[(self.h - i - 1)][c] = p
-                didplace = True
-                break
-        return didplace
+                return True
+        return False
 
     def game_over(self) -> int:
-        # to return a "0", "1", "2", or "3" indicating which player has won ("0" if no one one,
-        # "3" if no more room on board).
+        """Returns whether the game is over and in what state
+
+        Returns:
+            int: 0 for not over, 1 for P1 won, 2 for P2 won, 3 for tie
+        """
         if self.is_winner(1):
             return 1
         else:
@@ -87,9 +139,13 @@ class GameBoard:
                     return 0
 
     def is_winner(self, p: int) -> bool:
-        """
-        Returns True or False if player (p) has won.
-        p is the player (1 or 2)
+        """Returns true or false based on whether the player has won
+
+        Args:
+            p (int): Player 1 or 2
+
+        Returns:
+            bool: Whether the player has won or not
         """
         # Horizontal
         for i in range(self.h):
